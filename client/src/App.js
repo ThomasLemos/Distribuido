b91f1dee2a473f12971be881f5a5ddc1c2d7fb2c
Thomas <thomas.mlemos@gmail.com>
2023-04-01 18:26:56 -0300

    gravar audio

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,69 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState } from 'react';
 
 function App() {
+  const [recording, setRecording] = useState(false);
+  const [duration, setDuration] = useState(5);
 
-    const [data, setData] = useState([{}])
+  // Function to handle starting the audio recording
+  const startRecording = async () => {
+    try {
+      // Send POST request to Flask endpoint to start recording
+      const response = await fetch('/start-recording', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ duration }),
+      });
 
-    useEffect(() => {
-        fetch("/members").then(
-            res => res.json()
-        ).then(
-            data => {
-                setData(data)
-                console.log(data)
-            }
-        )
-      }, [])
+      // Update recording state to true
+      setRecording(true);
+    } catch (error) {
+      console.error('Error starting recording:', error);
+    }
+  };
+
+  // Function to handle stopping the audio recording
+  const stopRecording = async () => {
+    try {
+      // Send POST request to Flask endpoint to stop recording
+      const response = await fetch('/stop-recording', {
+        method: 'POST',
+      });
+
+      // Update recording state to false
+      setRecording(false);
+    } catch (error) {
+      console.error('Error stopping recording:', error);
+    }
+  };
 
   return (
     <div>
+      <h1>Record Audio</h1>
 
-        {(typeof data.members === 'undefined') ? (
-            <p>Loading...</p>
-        ) : (
-            data.members.map((member, i) => (
-                <p key={i}>{member}</p>
-            ))
-        )}
+      <div>
+        <label htmlFor="duration-input">Duration (seconds):</label>
+        <input
+          type="number"
+          id="duration-input"
+          value={duration}
+          onChange={(event) => setDuration(event.target.value)}
+        />
+      </div>
 
+      {!recording && (
+        <button onClick={startRecording}>Start Recording</button>
+      )}
 
-
+      {recording && (
+        <button onClick={stopRecording}>Stop Recording</button>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,45 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import soundfile as sf
+import numpy as np
+import sounddevice as sd
 
 app = Flask(__name__)
 
-@app.route('/members')
-def hello():
-    return ({'members': ['members1', 'members2', 'members3',]})
+def record_audio(duration):
+    # Record audio using sounddevice library
+    fs = 44100  # Sample rate
+    frames = int(duration * fs)  # Number of frames
+    recording = sd.rec(frames, samplerate=fs, channels=1)
+
+    # Wait until recording is finished
+    sd.wait()
+
+    # Save recording to WAV file
+    sf.write('audio/recorded_audio.wav', recording, fs)
+
+def stop_recording():
+    # Stop recording by stopping the sounddevice stream
+    sd.stop()
+
+@app.route('/start-recording', methods=['POST'])
+def start_recording():
+    # Get duration of recording in seconds from client request
+    duration = request.json['duration']
+
+    # Start recording audio
+    record_audio(duration)
+
+    return jsonify({'message': 'Audio recording started.'})
+
+@app.route('/stop-recording', methods=['POST'])
+def end_recording():
+    # Stop recording audio
+    stop_recording()
+
+    return jsonify({'message': 'Audio recording stopped.'})
 
 if __name__ == '__main__':
     app.run()
+
+
 
